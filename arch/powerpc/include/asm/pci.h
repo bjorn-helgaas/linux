21ead817ea0b695f11ae67a1d9c8c4614b5b54e8
Bjorn Helgaas <bhelgaas@google.com>
2012-03-07 09:36:29 -0700

    powerpc/PCI: get rid of device resource fixups
    Tell the PCI core about host bridge address translation so it can take
    care of bus-to-resource conversion for us.
    
    Previously, powerpc used a HEADER quirk to perform this address fixup
    in the following path:
    
        pci_device_add()
          pci_fixup_device(pci_fixup_header, dev)
            pcibios_fixup_resources()
    	  fixup_resource()
    
    This happened for both generic (via pci_scan_child_bus()) and
    OF device tree enumeration (via of_scan_bus()).
    
    This patch removes fixup_resource(), so the HEADER quirk no longer does
    address fixup.  The generic path now does the fixup in __pci_read_base()
    using the offsets supplied with pci_add_resource_offset().  The OF path
    now does the fixup with new pcibios_bus_to_resource() calls in
    of_pci_parse_addrs() and of_scan_pci_bridge().
    
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    CC: linuxppc-dev@lists.ozlabs.org
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- arch/powerpc/include/asm/pci.h
+++ arch/powerpc/include/asm/pci.h
@@ -154,13 +154,7 @@ extern int pci_mmap_legacy_page_range(struct pci_bus *bus,
 
 #endif /* CONFIG_PPC64 */
 
-extern void pcibios_resource_to_bus(struct pci_dev *dev,
-			struct pci_bus_region *region,
-			struct resource *res);
-
-extern void pcibios_bus_to_resource(struct pci_dev *dev,
-			struct resource *res,
-			struct pci_bus_region *region);
+#define ARCH_HAS_GENERIC_PCI_OFFSETS
 
 extern void pcibios_claim_one_bus(struct pci_bus *b);
 
@@ -190,6 +184,7 @@ extern void pci_resource_to_user(const struct pci_dev *dev, int bar,
 				 const struct resource *rsrc,
 				 resource_size_t *start, resource_size_t *end);
 
+extern resource_size_t pcibios_io_space_offset(struct pci_controller *hose);
 extern void pcibios_setup_bus_devices(struct pci_bus *bus);
 extern void pcibios_setup_bus_self(struct pci_bus *bus);
 extern void pcibios_setup_phb_io_space(struct pci_controller *hose);
